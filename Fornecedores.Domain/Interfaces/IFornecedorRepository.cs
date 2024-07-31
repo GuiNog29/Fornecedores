d4ea5af78767fdba2c0f0be8a614914f3c4edde6
Guilherme Almeida <guimathiasgui@gmail.com>
2024-07-31 02:04:28 -0300

    Criando camada de dominio

---
+++ Fornecedores.Domain/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fornecedores.Domain.Entities;
+
+namespace Fornecedores.Domain.Interfaces
+{
+    public interface IFornecedorRepository
+    {
+        Task<Fornecedor> CadastrarFornecedor(Fornecedor fornecedor);
+        Task<Fornecedor> AtualizarFornecedor(Fornecedor fornecedor, int fornecedorId);
+        Task<bool> ExcluirFornecedorPorId(int fornecedorId);
+        Task<Fornecedor> BuscarFornecedorPorId(int fornecedorId);
+        Task<IEnumerable<Fornecedor>> BuscarTodosFornecedores();
+    }
+}
---
+++ Fornecedores.Infrastructure/Data/FornecedoresDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Fornecedores.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fornecedores.Infrastructure.Data
+{
+    public class FornecedoresDbContext : DbContext
+    {
+        public FornecedoresDbContext(DbContextOptions<FornecedoresDbContext> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
